0af89125101a93e999adbf2aa1c86a7e5c11953d
Silfi Nazarina <silfinazarina@gmail.com>
2024-02-22 19:46:58 +0700

    first commit

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pyung Store</title>
+</head>
+<body>
+    <h3>Welcome!</h3> 
+    Let's start your shopping journey with us
+</body>
+</html>
